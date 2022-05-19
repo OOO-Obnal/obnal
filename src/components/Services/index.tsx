6db299ed6cb5d60da365bae19f3237889778c8c9
Harwer <HarwerPlay@yandex.ru>
2022-05-19 14:58:23 +0700

    as

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./Services.scss";
 
 const Services = () => {
-  return <div>наши мега услуги </div>;
+  return (
+    <div className="services">
+      <ul className="services__ul">
+        <h1 className="services__h">Наши услуги :</h1>
+        <li className="services__li">Обнал без мыла</li>
+        <li className="services__li">Обнал с мылом</li>
+        <li className="services__li">Обнал по-чёрному</li>
+        <li className="services__li">Обнал по-белому</li>
+        <li className="services__li">Обнал через нал</li>
+        <li className="services__li">Обнал по вене</li>
+      </ul>
+      <ul className="services__ul uo">
+        <h1 className="services__h">Цена :</h1>
+        <li className="services__li ko">500 ETH</li>
+        <li className="services__li ko">600 ETH</li>
+        <li className="services__li ko">300 ETH</li>
+        <li className="services__li ko">800 ETH</li>
+        <li className="services__li ko">1000 ETH</li>
+        <li className="services__li ko">9000 ETH</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Services;
